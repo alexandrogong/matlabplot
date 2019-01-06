b12b4c8c5b8dd5a9315f701ed74cefc0ec2d9989
Alexandro Gong <766842290@qq.com>
2019-01-06 23:01:30 +0800

    中国平安股票分析demo

--- alexandrocode/tushare_demo.py
+++ alexandrocode/tushare_demo.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 
 # 返回值说明：
+# get_hist_data
 # date：日期
 # open：开盘价
 # high：最高价
@@ -21,31 +22,70 @@ import pandas as pd
 # v_ma20:20日均量
 # turnover:换手率[注：指数无此项]
 
+# get_today_all
+# code：代码
+# name:名称
+# changepercent:涨跌幅
+# trade:现价
+# open:开盘价
+# high:最高价
+# low:最低价
+# settlement:昨日收盘价
+# volume:成交量
+# turnoverratio:换手率
+# amount:成交量
+# per:市盈率
+# pb:市净率
+# mktcap:总市值
+# nmc:流通市值
+
 if __name__ == '__main__':
     # 提供中文字符支持
     pl.rcParams['font.sans-serif'] = ['SimHei']
 
-    # 获取中国平安每日成交信息
-    data = ts.get_hist_data('601318', start='2018-01-01', end='2019-03-27')
+    # 显示所有列
+    pd.set_option('display.max_columns', None)
 
-    open = data['open']
-    high = data['high']
-    close = data['close']
-    low = data['low']
-    volume = data['volume']
-    price_change = data['price_change']
-    p_change = data['p_change']
-    ma5 = data['ma5']
-    ma10 = data['ma10']
-    ma20 = data['ma20']
-    v_ma5 = data['v_ma5']
-    v_ma10 = data['v_ma10']
-    v_ma20 = data['v_ma20']
+    # =====获取中国平安一个时间段的历史信息=====
+    # data = ts.get_hist_data('601318', start='2018-01-01', end='2019-03-27')
+    #
+    # open = data['open']
+    # high = data['high']
+    # close = data['close']
+    # low = data['low']
+    # volume = data['volume']
+    # price_change = data['price_change']
+    # p_change = data['p_change']
+    # ma5 = data['ma5']
+    # ma10 = data['ma10']
+    # ma20 = data['ma20']
+    # v_ma5 = data['v_ma5']
+    # v_ma10 = data['v_ma10']
+    # v_ma20 = data['v_ma20']
+    #
+    # '''分析中国平安每日成交相关性'''
+    # plt.figure(figsize=[10, 6])
+    # plt.scatter(v_ma10, p_change)
+    # plt.show()
 
-    # 分析中国平安每日成交相关性
-    plt.figure(figsize=[10, 6])
-    plt.scatter(v_ma10, p_change)
+    # =====获取实时行情数据=====
+    filename = '实时行情数据'
+    filename = filename + '.csv'
+    data = ts.get_today_all()
+
+    data.to_csv(filename)
+
+    with open(filename, 'w') as f:
+        f.write(data)
+
+    per = data['per']
+    trade = data['trade']
+
+    plt.figure(figsize=[8, 6])
+    plt.plot(per, trade)
     plt.show()
+
+
 
 
 
